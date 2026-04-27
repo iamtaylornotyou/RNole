08efc7ebe0bc7dd38049015aba11ceb48588870d
iamtaylornotyou <iamtaylornotyou@gmail.com>
2026-04-27 12:58:44 -0400

    added script to run with AnoCar reference

---
+++ config/pace_phoenix_car.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=rnole_pipeline
+#SBATCH --account=gts-jstroud36
+#SBATCH --partition=cpu-large
+#SBATCH --qos=inferno
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=24
+#SBATCH --mem=128G
+#SBATCH --time=24:00:00
+#SBATCH --output=logs/rnole_%j.out
+#SBATCH --error=logs/rnole_%j.err
+
+# Activate conda environment
+eval "$(/storage/project/r-jstroud36-0/tcooper84/miniconda3/bin/conda shell.bash hook)"
+conda activate rnole-hpc2
+
+# Run pipeline
+cd /storage/project/r-jstroud36-0/tcooper84/RNole
+
+nextflow run pipeline/main.nf \
+    --input samplesheets/test_1M.csv \
+    --fasta ref/AnoCar.fna.gz \
+    --gtf ref/AnoCar.gtf.gz \
+    --outdir results/test_1M_salmon_car \
+    --pseudo_aligner salmon \
+    --skip_alignment \
+    -profile singularity \
+    -c config/pace_phoenix.config
