e11e9521b4e5532fcd35e71b04ed442863bf954c
taylor <iamtaylornotyou@gmail.com>
2026-04-29 21:53:43 -0400

    removed boolean

--- pipeline/bin/run_rnaseq.sh
+++ pipeline/bin/run_rnaseq.sh
@@ -15,7 +15,7 @@ nextflow run "${rnaseq_pipeline}" \
     --gtf "${ref_path}/${bn}.gtf.gz" \
     --outdir "${outdir}/${bn}" \
     --pseudo_aligner salmon \
-    --skip_alignment true \
+    --skip_alignment \
     --salmon_index "${ref_path}/${bn}_salmon_index" \
     -profile "${profile}" \
     -c "${config}"
