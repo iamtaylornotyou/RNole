fd8478c294b98d41f55e4297e12f97801f16ea2b
taylor <iamtaylornotyou@gmail.com>
2026-05-03 15:52:52 -0400

    updated file to tag isoforms

--- pipeline/bin/rename_fasta_headers.py
+++ pipeline/bin/rename_fasta_headers.py
@@ -4,6 +4,7 @@
 import re
 import sys
 import os
+from collections import defaultdict
 
 input_dir  = sys.argv[1]
 output_dir = sys.argv[2]
@@ -15,12 +16,19 @@ for filename in os.listdir(input_dir):
         input_file  = os.path.join(input_dir, filename)
         output_file = os.path.join(output_dir, filename)
         
+        seen = defaultdict(int)
+
         with open(input_file) as f_in, open(output_file, 'w') as f_out:
             for line in f_in:
                 if line.startswith('>'):
                     match = re.search(r'\[locus_tag=([^\]]+)\]', line)
                     if match:
-                        f_out.write(f'>{match.group(1)}\n')
+                        locus_tag = match.group(1)
+                        seen[locus_tag] += 1
+                        if seen[locus_tag] > 1:
+                            f_out.write(f'>{locus_tag}_isoform{seen[locus_tag]}\n')
+                        else:
+                            f_out.write(f'>{locus_tag}\n')
                     else:
                         f_out.write(line)
                 else:
