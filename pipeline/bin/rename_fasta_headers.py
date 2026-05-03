b8a5f789f198f88e14b979b47b7b3762b091dba6
taylor <iamtaylornotyou@gmail.com>
2026-05-03 15:37:10 -0400

    changed script to accept directory instead of file

--- pipeline/bin/rename_fasta_headers.py
+++ pipeline/bin/rename_fasta_headers.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python3
-# usage: ./rename_fasta_headers.py <input.faa> <output.faa>
+# usage: ./rename_fasta_headers.py <input_dir> <output_dir>
 
 import re
 import sys
+import os
 
-input_file  = sys.argv[1]
-output_file = sys.argv[2]
+input_dir  = sys.argv[1]
+output_dir = sys.argv[2]
 
-with open(input_file) as f_in, open(output_file, 'w') as f_out:
-    for line in f_in:
-        if line.startswith('>'):
-            match = re.search(r'\[locus_tag=([^\]]+)\]', line)
-            if match:
-                f_out.write(f'>{match.group(1)}\n')
-            else:
-                f_out.write(line)  # keep original if no locus_tag found
-        else:
-            f_out.write(line)
+os.makedirs(output_dir, exist_ok=True)
+
+for filename in os.listdir(input_dir):
+    if filename.endswith('.faa'):
+        input_file  = os.path.join(input_dir, filename)
+        output_file = os.path.join(output_dir, filename)
+        
+        with open(input_file) as f_in, open(output_file, 'w') as f_out:
+            for line in f_in:
+                if line.startswith('>'):
+                    match = re.search(r'\[locus_tag=([^\]]+)\]', line)
+                    if match:
+                        f_out.write(f'>{match.group(1)}\n')
+                    else:
+                        f_out.write(line)
+                else:
+                    f_out.write(line)
