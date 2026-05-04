5b32508450c2f3326d53165930e8836b94c54cf7
taylor <iamtaylornotyou@gmail.com>
2026-05-04 13:41:58 -0400

    updated shell scripts to run full pipeline on PACE

---
+++ config/pace_phoenix_yeast.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=rnole_pipeline
+#SBATCH --account=gts-jstroud36
+#SBATCH --partition=cpu-large
+#SBATCH --qos=inferno
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=24
+#SBATCH --mem=128G
+#SBATCH --time=24:00:00
+#SBATCH --output=logs/rnole_%j.out
+#SBATCH --error=logs/rnole_%j.err
+
+# Activate conda environment
+eval "$(/storage/project/r-jstroud36-0/tcooper84/miniconda3/bin/conda shell.bash hook)"
+conda activate rnole-hpc
+
+# Run pipeline
+cd /storage/project/r-jstroud36-0/tcooper84/RNole
+
+NXF_VER=25.10.4 nextflow run pipeline/main.nf \
+    --input samplesheets/yeast_multi_ref_pace.csv \
+    --outdir 'results/yeast_test_pace' \
+    --container_engine 'singularity' \
+    --rnaseq_config 'config/pace_phoenix.config' \
+    --orthofinder 'proteome/' \
+    --gene_names_from SchPom \
+    -c 'config/pace_phoenix.config' \
+    -profile 'singularity'
